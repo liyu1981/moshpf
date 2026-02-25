4984abed52d2e65d816d58bd5f3f405660f47107
Yu Li <liyu1981@gmail.com>
2026-02-25 23:14:37 +1100

    feat: refine syslog integration and silence info logs on console fallback

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -15,14 +15,19 @@ func Init() {
 	if os.Getenv("APP_ENV") == "dev" {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 		zerolog.SetGlobalLevel(zerolog.TraceLevel)
-	} else {
-		w, err := syslog.New(syslog.LOG_INFO|syslog.LOG_USER, "moshpf")
-		if err != nil {
-			// Fallback to stderr if syslog fails
-			zerolog.SetGlobalLevel(zerolog.InfoLevel)
-			return
-		}
+		return
+	}
+
+	// Non-dev mode: try syslog
+	w, err := syslog.New(syslog.LOG_INFO|syslog.LOG_USER, "moshpf")
+	if err == nil {
+		// Direct zerolog output to syslog writer with level mapping
 		log.Logger = zerolog.New(zerolog.SyslogLevelWriter(w))
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	} else {
+		// Fallback to a quiet console output if syslog fails.
+		// We use WarnLevel to keep the CLI clean of Info/Debug logs.
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.Kitchen})
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
 	}
 }
